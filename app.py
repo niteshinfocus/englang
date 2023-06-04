7663a21b719eace0366f526e994bb7e224b0ff26
nitesh <109079817+niteshinfocus@users.noreply.github.com>
2023-06-04 12:41:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import *
 import openai
 import os
-from script import random_script
-from englisttohindi.englisttohindi import EngtoHindi
+from script import engScript, hinScript
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
@@ -10,9 +9,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    msg=random_script
-    trans = EngtoHindi(message=msg)
-    return render_template('index.html', translated=trans.convert)
+    msg=hinScript
+    return render_template('index.html', translated=msg)
 
 
 @app.route('/search', methods=['POST'])
@@ -22,13 +20,11 @@ def result():
     userInput = '{}'.format(text)
 
     #generating response
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "compare this sentence '" + userInput +" '" + " with '" + random_script + " ' "+ " and find the areas in which first sentence is wrong"}])
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "compare this sentence '" + userInput +" '" + " with '" + engScript + " ' "+ " and find the areas in which first sentence is wrong with respect to second sentence. Tell in 2-3 lines"}])
     aiMsg = completion.choices[0].message.content
 
-    hindi_feedback = EngtoHindi(message=aiMsg)
-    t = hindi_feedback.convert
     
-    return render_template('result.html', userInput=userInput, hiMsg=t, aiMsg=aiMsg, random_script=random_script)
+    return render_template('result.html', userInput=userInput, aiMsg=aiMsg, random_script=engScript)
 
 
 
